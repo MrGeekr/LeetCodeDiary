4a3fc6325130cdaf016c0b7eccb35a688a24a94d
fateYuHuaii <939220189@qq.com>
2019-07-28 14:43:34 +0800

    commit  "1. Two Sum"

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Mr.Xu.
+ * Date : 2019/7/28.
+ * Description :1. 两数之和
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i <nums.length; i++) {
+            int result=target-nums[i];
+            if (map.containsKey(result)){
+                return new int[]{i,map.get(result)};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+}
