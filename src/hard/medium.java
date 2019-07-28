1a1ce85c3c0334f8364fb695024a53d7c39e78a2
fateYuHuaii <939220189@qq.com>
2019-07-28 11:04:22 +0800

    create the LeetCodeDiary projecat and commit

---
+++ src/hard/medium.java
@@ -0,0 +1,9 @@
+package hard;
+
+/**
+ * Created by Mr.Xu.
+ * Date : 2019/7/28.
+ * Description :
+ */
+public class medium {
+}
---
+++ src/medium/hard.java
@@ -0,0 +1,9 @@
+package medium;
+
+/**
+ * Created by Mr.Xu.
+ * Date : 2019/7/28.
+ * Description :
+ */
+public class hard {
+}
