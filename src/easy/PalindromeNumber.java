35061294f37a32d6839cdb342a4e858c4e291e7b
fateYuHuaii <939220189@qq.com>
2019-07-29 20:26:42 +0800

    change the class name

--- src/easy/PalindromeNumber.java
+++ src/easy/PalindromeNumber.java
@@ -3,9 +3,9 @@ package easy;
 /**
  * Created by Mr.Xu.
  * Date : 2019/7/29.
- * Description :
+ * Description :9. 回文数
  */
-public class PalindromeNumber9 {public boolean isPalindrome(int x) {
+public class PalindromeNumber {public boolean isPalindrome(int x) {
     String string=new StringBuilder(String.valueOf(x)).reverse().toString();
     return string.equals(String.valueOf(x));
 }}
